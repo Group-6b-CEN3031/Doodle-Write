afc5951cc865f5899d3dbaa6c0586b42b4b56766
Tahreak Robinson <tahreak@Tahreaks-MacBook-Pro.local>
2019-11-05 17:05:21 -0500

    Added config.example.js for config location

---
+++ server/config.example.js
@@ -0,0 +1,11 @@
+//This file holds any configuration variables we may need
+//"config.js" is usually ignored by git to protect sensitive information, such as your database"s username and password
+
+module.exports = {
+    db: {
+        // typical read only user
+        uri: "",
+        // admin user that can read/write to the database
+        adminuri: ""
+    }
+};
