8970637b4b16967cd8ca167110123ec898750be3
mprovosty <mprovosty@ufl.edu>
2019-11-05 13:37:25 -0500

    moved express.js and resolving import errors

--- client/src/pages/Shop.js
+++ client/src/pages/Shop.js
@@ -3,18 +3,22 @@ import React from "react";
 import Header from "./../components/Header";
 import Footer from "./../components/Footer";
 
+import shopitems from "./../assets/shop.json";
+
 class Shop extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
 
         }
+        var shop = JSON.parse(shopitems)
     }
-    render() { 
+    render() {
         return (
             <React.Fragment>
                 <Header tab="Shop"/>
                 Shop
+                { shop.items[0].name }
                 <Footer/>
             </React.Fragment>
         )
--- server/config/express.js
+++ server/config/express.js
@@ -3,14 +3,14 @@ const path = require("path"),
     mongoose = require("mongoose"),
     morgan = require("morgan"),
     bodyParser = require("body-parser"),
-    exampleRouter = require("./routes/examples.server.routes");
+    exampleRouter = require("../routes/examples.server.routes");
 
 module.exports.init = () => {
     /* 
         connect to database
         - reference README for db uri
     */
-    mongoose.connect(process.env.DB_URI || require("./config").db.uri, {
+    mongoose.connect(process.env.DB_URI || require("./config.js").db.uri, {
         useNewUrlParser: true
     });
     mongoose.set("useCreateIndex", true);
