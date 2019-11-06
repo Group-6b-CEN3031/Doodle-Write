9651e154d78f212bb0169b69b89a832274b3ba6b
trob0697 <46656573+trob0697@users.noreply.github.com>
2019-11-06 12:55:04 -0500

    Delete config.js

--- server/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    db: {
-        // typical read only user
-        uri: "mongodb+srv://displayshop:ZRlaSYQHumsVC3HH@cen3031-c3zdn.mongodb.net/test?retryWrites=true&w=majority",
-        // admin user that can read/write to the database
-        adminuri: "mongodb+srv://admin:UM1YV3IQJbgMMrzP@cen3031-c3zdn.mongodb.net/test?retryWrites=true&w=majority"
-    }
-};
