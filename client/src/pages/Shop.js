251d722cb8949850424ea74b77da878b9d72f1d8
mprovosty <mprovosty@ufl.edu>
2019-11-13 22:34:18 -0500

    removed reference to the json containing item information

--- client/src/pages/Shop.js
+++ client/src/pages/Shop.js
@@ -3,7 +3,6 @@ import React from "react";
 import Header from "./../components/Header";
 import Footer from "./../components/Footer";
 
-import itemDB from "./../assets/items.json";
 import api from "../api";
 
 class Shop extends React.Component {
