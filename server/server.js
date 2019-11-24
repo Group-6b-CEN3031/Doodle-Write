f4064a83ad7ac46ef6ec84ad4f37bb1e33448402
Tahreak Robinson <tahreak@Tahreaks-MacBook-Pro.local>
2019-11-23 20:25:56 -0500

    Zzzz

--- server/server.js
+++ server/server.js
@@ -2,8 +2,7 @@ const path = require('path'),
     express = require('express'),
     mongoose = require('mongoose'),
     morgan = require('morgan'),
-    bodyParser = require('body-parser'),
-    stripe = require('stripe')(require('./config').stripe.secret);
+    bodyParser = require('body-parser');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -19,28 +18,12 @@ mongoose.connect(process.env.DB_URI || require('./config').db.uri, {
     useFindAndModify: false
 });
 
-// connect to stripe
-(async () => {
-    const session = await stripe.checkout.sessions.create({
-        payment_method_types: ['card'],
-        line_items: [{
-        name: 'T-shirt',
-        description: 'Comfortable cotton t-shirt',
-        images: ['https://example.com/t-shirt.png'],
-        amount: 500,
-        currency: 'usd',
-        quantity: 1,
-        }],
-        success_url: 'https://example.com/success?session_id={CHECKOUT_SESSION_ID}',
-        cancel_url: 'https://example.com/cancel',
-    });
-})();
-
 // enable request logging for development debugging
 app.use(morgan("dev"));
 
 // add a router
 app.use('/shop/data', require('./routes/shop.router.js'));
+app.use('/shop/order', require('./routes/order.router'));
 
 if (process.env.NODE_ENV === 'production') {
     // Serve any static files
