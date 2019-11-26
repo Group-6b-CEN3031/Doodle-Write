8776b5e48d533376da3bb3d36d2d285b2ed2fd38
Dylan Bloyer <dbloyer@ufl.edu>
2019-11-26 14:07:08 -0500

    Initial attempt to change testimonials from strings into database objects

--- client/src/pages/Testimonials.js
+++ client/src/pages/Testimonials.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {Container, Row, Col} from 'react-bootstrap';
+import api from "../api";
 
 import Header from "./../components/Header";
 import Footer from "./../components/Footer";
@@ -7,6 +8,40 @@ import Footer from "./../components/Footer";
 import quotes from "./../assets/quotes.jpg";
 
 class Testimonials extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            testimonal: [],
+        }
+    }
+
+    componentWillMount = async() => {
+        await api.displayAll().then (testimonal => {
+            this.state ({
+                testimonal: testimonal.data.data,
+            })
+        })
+    }
+
+    displayTestimonials(testimonial){
+        if (testimonial.length === 0){
+            return (
+            <ul className="text-center" style={{color: "#5e5e5e"}}>
+                <li>No testimnoials currently stored.</li>
+            </ul>
+            )
+        }
+        return (
+        <ul className="text-left" style={{color: "#5e5e5e"}}>
+            <li>{testimonial.content} " -" {testimonial.name} " " {testimonial.credential}</li>
+            <li>"I was pleasantly surprised to see how it captured and kept my students attention." -Theresa P. Special Education Teacher</li>
+            <li>"My younger art students were able to participate more confidently as they accomplished tasks with the support of the tool that were challenging without it. It brought an instant sense of satisfaction of the kids." -James M. Art Teacher</li>
+            <li>"I loved the excitement that showed on my class's face as I showed them what we were using in class that day." -Sharon T. Kindergarten Teacher</li>
+            <li>"I found myself using it after my client had left for my boardwork. It was comfortable to use and fun!" -Douglene J. Occupational Therapist</li>
+        </ul>
+        )
+    }
+
     render() { 
         return (
             <React.Fragment>
@@ -17,20 +52,15 @@ class Testimonials extends React.Component {
                 			<p style={styles.h1Text}>WHAT DO PEOPLE THINK?</p>
                 		</div>
                 	</div>
-                	<Row>
-                		<Col className="align-items-center">
-                			<img src={quotes} alt="quotation marks" class="shadow-lg" style={{width: "100%"}}/>
-                		</Col>
-                		<Col className="d-flex flex-column">
-                			<div style={{...styles.grayLine, width: 220}}/>
-                			<ul className="text-left" style={{color: "#5e5e5e"}}>
-                				<li>"I was pleasantly surprised to see how it captured and kept my students attention." -Theresa P. Special Education Teacher</li>
-                				<li>"My younger art students were able to participate more confidently as they accomplished tasks with the support of the tool that were challenging without it. It brought an instant sense of satisfaction of the kids." -James M. Art Teacher</li>
-                				<li>"I loved the excitement that showed on my class's face as I showed them what we were using in class that day." -Sharon T. Kindergarten Teacher</li>
-                				<li>"I found myself using it after my client had left for my boardwork. It was comfortable to use and fun!" -Douglene J. Occupational Therapist</li>
-                			</ul>
-                		</Col>
-                	</Row> 
+                    <Row>
+                        <Col className="align-items-center">
+                            <img src={quotes} alt="quotation marks" class="shadow-lg" style={{width: "100%"}}/>
+                        </Col>
+                        <Col className="d-flex flex-column">
+                            <div style={{...styles.grayLine, width: 220}}/>
+                            <displayTestimonials testimonial={this.state.testimonial} /> 
+                        </Col>
+                    </Row>
                 </Container>
                 <div style={{height: 100}}/>
                 <Footer/>
