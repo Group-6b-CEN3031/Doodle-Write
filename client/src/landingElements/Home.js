957374521021467c590eee8125c97e43059f7ee4
Tahreak Robinson <tahreak@Tahreaks-MacBook-Pro.local>
2019-11-27 15:14:40 -0500

    Home spacing

--- client/src/landingElements/Home.js
+++ client/src/landingElements/Home.js
@@ -21,9 +21,10 @@ class Home extends React.Component {
                     </div>
                     <Row style={{padding: 75}}>
                         <Col sm={4} className='align-self-center'>
-                            <img src={AdultWithKids} alt='Blue Grip' className='shadow-lg' style={{width: 300}}/>    
+                            <img src={AdultWithKids} alt='Blue Grip' className='shadow-lg' style={{width: 350}}/>    
                         </Col>
-                        <Col sm={8}>
+                        <Col sm={1}/>
+                        <Col sm={7}>
                             <Row className='justify-content-center'>
                                 <div style={{fontFamily: 'Arial Rounded MT Bold', fontSize: 20, letterSpacing: 8, textAlign: 'center', maxWidth: 400}}>FUN AND ENGAGING WRITING SUPPORT !</div>
                             </Row>
@@ -31,7 +32,7 @@ class Home extends React.Component {
                                 <div style={{backgroundColor: '#000000', height: 1, width: 88, margin: 14}}/>
                             </Row>
                             <Row className='justify-content-center'>
-                                <div style={{fontFamily: 'Arial', color: '#404040', fontSize: 16, textAlign: 'left', width: 800}}>Used by pre-writers, differently developing doodlers and even adults! The earlier children master written communication skills the better they do academically. Handwriting is the number one referral to Occupational Therapist in elementary schools. Often, the reason that older children struggle with handwriting are related to the fundamental skills needed to write NOT just grasp.</div>
+                                <div style={{fontFamily: 'Arial', color: '#404040', fontSize: 16, textAlign: 'left'}}>Used by pre-writers, differently developing doodlers and even adults! The earlier children master written communication skills the better they do academically. Handwriting is the number one referral to Occupational Therapist in elementary schools. Often, the reason that older children struggle with handwriting are related to the fundamental skills needed to write NOT just grasp.</div>
                             </Row>
                         </Col>
                     </Row>
