3264b6077c870b06e27a066101f8b06aebd6bdbb
keethsmith <keethsmith@ufl.edu>
2019-11-26 19:24:46 -0500

    heading and paragraph removal

--- client/src/pages/Media.js
+++ client/src/pages/Media.js
@@ -21,8 +21,8 @@ class Media extends React.Component {
                             alt="First slide"
                             />
                             <Carousel.Caption>
-                            <h3>First slide label</h3>
-                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                            <h3></h3>
+                            <p></p>
                             </Carousel.Caption>
                         </Carousel.Item>
                         
@@ -34,8 +34,8 @@ class Media extends React.Component {
                             />
 
                             <Carousel.Caption>
-                            <h3>Second slide label</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            <h3></h3>
+                            <p></p>
                             </Carousel.Caption>
                         </Carousel.Item>
                         <Carousel.Item>
@@ -46,8 +46,8 @@ class Media extends React.Component {
                             />
 
                             <Carousel.Caption>
-                            <h3>Third slide label</h3>
-                            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                            <h3></h3>
+                            <p></p>
                             </Carousel.Caption>
                         </Carousel.Item>
                     </Carousel>
