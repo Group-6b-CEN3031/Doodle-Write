5368863429c1b4257e04744eb38c867ffbd77893
Tahreak Robinson <tahreak@Tahreaks-MacBook-Pro.local>
2019-11-27 12:53:20 -0500

    Red grip fix

--- client/src/landingElements/Media.js
+++ client/src/landingElements/Media.js
@@ -3,16 +3,16 @@ import Carousel from 'react-bootstrap/Carousel';
 
 import Carousel1 from '../assets/Carousel1.jpeg';
 import Carousel2 from '../assets/Carousel2.jpeg';
-//import redGrip from '../assets/.jpeg';
+import redGrip from '../assets/redGrip.jpeg';
 
 class Media extends React.Component {
     render() { 
         return (
             <div id='Media' style={{backgroundColor: 'white', textAlign: 'center'}}>
                 <Carousel style={{padding: 50, width:750, display:"inline-block"}}>
-                    {/* <Carousel.Item>
+                    <Carousel.Item>
                         <img src={redGrip} alt="First slide" className="d-block w-100" />
-                    </Carousel.Item> */}
+                    </Carousel.Item>
                     <Carousel.Item>
                         <img src={Carousel1} alt="Third slide" className="d-block w-100"/>
                     </Carousel.Item>
