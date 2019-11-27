6e21866c277001df27917ae27e78d9cb0bec3aad
Tahreak Robinson <tahreak@Tahreaks-MacBook-Pro.local>
2019-11-27 12:51:31 -0500

    Merge Fixes

--- client/src/landingElements/Media.js
+++ client/src/landingElements/Media.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+
+import Carousel1 from '../assets/Carousel1.jpeg';
+import Carousel2 from '../assets/Carousel2.jpeg';
+//import redGrip from '../assets/.jpeg';
 
 class Media extends React.Component {
     render() { 
         return (
-            <div id='Media' style={{backgroundColor: 'white'}}>
-                Media
-                <div style={{height: 500}}/>
+            <div id='Media' style={{backgroundColor: 'white', textAlign: 'center'}}>
+                <Carousel style={{padding: 50, width:750, display:"inline-block"}}>
+                    {/* <Carousel.Item>
+                        <img src={redGrip} alt="First slide" className="d-block w-100" />
+                    </Carousel.Item> */}
+                    <Carousel.Item>
+                        <img src={Carousel1} alt="Third slide" className="d-block w-100"/>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img src={Carousel2} alt="Third slide" className="d-block w-100"/>
+                    </Carousel.Item>
+                </Carousel>
             </div>
         )
     }
