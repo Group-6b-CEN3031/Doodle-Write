321d5952185551ea6ac3c735db86072bdc6b77a5
Tahreak Robinson <tahreak@Tahreaks-MacBook-Pro.local>
2019-11-28 17:21:24 -0500

    Style fixes

--- client/src/landingElements/Testimonials.js
+++ client/src/landingElements/Testimonials.js
@@ -24,13 +24,11 @@ class Testimonials extends React.Component {
 
     render() {
         return (
-            <div id='Testimonials' style={{backgroundColor: 'white', opacity: 0.9, borderStyle: 'solid', borderWidth: 2, borderColor: '#63FF4A'}}>
-            {this.state.testimonials.length
-            ?
+            <div id='Testimonials' style={{backgroundColor: 'white', opacity: 0.9, borderStyle: 'solid', borderWidth: 2, borderColor: '#63FF4A', paddingBottom: 75}}>
                 <Container className="text-center" style={{width: "100%"}}>
                     <div className="row justify-content-center">
-                        <div style={styles.header}>
-                            <p style={styles.h1Text}>WHAT DO PEOPLE THINK?</p>
+                        <div style={{textAlign: 'center', fontFamily: 'Arial Rounded MT Bold', paddingTop: 75, paddingBottom: 20}}>
+                            WHAT DO PEOPLE THINK?
                         </div>
                     </div>
                     <Row>
@@ -38,9 +36,11 @@ class Testimonials extends React.Component {
                             <img src={quotes} alt="quotation marks" class="shadow-lg" style={{width: "100%"}}/>
                         </Col>
                         <Col className="d-flex flex-column">
-                            <div style={{...styles.grayLine, width: 220}}/>
-                            {this.state.testimonials.map((item) => {
-                            return (
+                        {this.state.testimonials
+                        ?
+                            <div style={{textAlign: 'center'}}>No testimnoials currently available.</div>
+                        :
+                            this.state.testimonials.map((item) => { return (
                                 <Col xs={12} md={4} style={{paddingTop: 75}}>
                                     <Card style={{width: "20rem", height: "36rem"}}>
                                         <Card.Body className="d-flex flex-column">
@@ -56,41 +56,14 @@ class Testimonials extends React.Component {
                                         </Card.Body>
                                     </Card>
                                 </Col>
-                            )})}
+                            )})
+                        }
                         </Col>
                     </Row>
                 </Container>
-            :
-                <div style={{textAlign: 'center', fontFamily: 'Arial', height: 500}}>
-                    No testimnoials currently available.
-                </div>
-            }
             </div>
         )
     }
 }
  
 export default Testimonials;
-
-const styles = {
-    sloganBar: {
-        background: "#307BFF",
-        color: "white",
-        textAlign: "center"
-    },
-    handsImg: {
-        maxWidth: "100%"
-    },
-    header: {
-        padding: 40,
-    },
-    h1Text: {
-        background: "black",
-        color: "white",
-        fontSize: 20,
-        padding: 10
-    },
-    h2Text: {
-        fontSize: 30
-    }
-}
