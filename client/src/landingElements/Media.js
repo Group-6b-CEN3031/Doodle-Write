6f2e6e7e1a8906d74de91940527571001d3fa2bf
keethsmith <keethsmith@ufl.edu>
2019-11-29 08:24:25 -0500

    changes to Media Carousel

--- client/src/landingElements/Media.js
+++ client/src/landingElements/Media.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Carousel from 'react-bootstrap/Carousel';
+import Slider from 'react-slick';
 
 import Carousel1 from '../assets/Carousel1.jpeg';
 import Carousel2 from '../assets/Carousel2.jpeg';
@@ -7,20 +8,57 @@ import redGrip from '../assets/redGrip.jpeg';
 
 class Media extends React.Component {
     render() { 
+        const settings = {
+            dots: true,
+            autoplay: true,
+            arrows: false,
+            focusOnSelect: true,
+            centerMode: true,
+            centerPadding: '50px',
+            adaptiveHeight: true,
+            accessibility: true,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 3,
+            slidesToScroll: 1
+          };
+
         return (
-            <div id='Media' style={{backgroundColor: 'white', opacity: 0.9, textAlign: 'center', borderStyle: 'solid', borderWidth: 2, borderColor: '#FF4AF4'}}>
-                <Carousel style={{marginBottom: '30px', marginTop: '30px', padding:'10px', width:750, display:"inline-block"}}>
-                    <Carousel.Item>
-                        <img src={redGrip} alt="First slide" className="d-block w-100" />
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img src={Carousel1} alt="Third slide" className="d-block w-100"/>
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <img src={Carousel2} alt="Third slide" className="d-block w-100"/>
-                    </Carousel.Item>
-                </Carousel>
+            <div>
+                <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
+                <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
+
+                <div id='Media' style={{paddingTop: '50px', paddingBottom:'50px', backgroundColor: 'white', opacity: 0.9, borderStyle: 'solid', borderWidth: 2, borderColor: '#FF4AF4'}}>
+
+                
+
+                        <Slider {...settings}>
+                            <div>
+                                <h3><img src={redGrip} /></h3>
+                            </div>
+                            <div>
+                                <h3><img src={redGrip} /></h3>
+                            </div>
+                            <div>
+                                <h3><img src={redGrip}  /></h3>
+                            </div>
+                            <div>
+                                <h3><img src={redGrip}  /></h3>
+                            </div>
+                            <div>
+                                <h3><img src={redGrip}  /></h3>
+                            </div>
+                            <div>
+                                <h3><img src={redGrip}  /></h3>
+                            </div>
+                        </Slider>
+
+                   
+
+                </div>
+            
             </div>
+        
         )
     }
 }
